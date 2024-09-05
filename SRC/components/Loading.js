486e18057aee560fc9273f8fb36f41b21b341523
Esther Nehemiah <105179203+CheeryCodes@users.noreply.github.com>
2024-09-06 00:37:16 +0100

    Create Loading.js

---
+++ SRC/components/Loading.js
@@ -0,0 +1,20 @@
+ 
+import React from 'react';
+ 
+ 
+function Loading() {
+ 
+  return (
+ 
+    <div>
+ 
+      <p>Pokeman List is Loading...</p>
+ 
+    </div>
+ 
+  );
+ 
+}
+ 
+ 
+export default Loading;
